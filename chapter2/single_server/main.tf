f0c6046df28e39e6bc7136ecac39efa1d0baf978
Diego Zarpelon <diego.zarpelon@hashicorp.com>
2025-01-22 15:33:35 +0100

    add auto scalling group and remote output of public ip

--- chapter2/single_server/main.tf
+++ chapter2/single_server/main.tf
@@ -4,47 +4,66 @@ provider "aws" {
 }
 
 # create single instance in aws
-resource "aws_instance" "example" {
-  ami = "ami-0fb653ca2d3203ac1"
+resource "aws_launch_configuration" "example" {
+  image_id      = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
   # make the instance part of the vpc securit group
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
   # run script to add data to index.html
   user_data = <<-EOF
     #!/bin/bash
     echo "Hello, World" > index.html
     nohup busybox httpd -f -p ${var.server_port} &
     EOF
-  user_data_replace_on_change = true
-#add tag to name the resource
-tags = {
-  name = "terraform-example"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
-}
+
 
 # create security group to allo port 8080
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   ingress {
-    from_port = var.server_port
-    to_port = var.server_port
-    protocol = "tcp"
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
+}
+
+#create autoscalling group
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+  min_size             = 2
+  max_size             = 10
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
 }
 
 # create variables
 
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
-  type = number
-  default = 8080
+  type        = number
+  default     = 8080
 }
 
-# create outputs
-output "public_ip" {
-  value = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
+#create data_sources
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
