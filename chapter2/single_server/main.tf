41f9011030697fc8b091958558690dbadf1dce03
Diego Zarpelon <diego.zarpelon@hashicorp.com>
2025-01-22 14:25:04 +0100

    create one t2.micro instance in us-east-2

--- chapter2/single_server/main.tf
+++ chapter2/single_server/main.tf
@@ -1,3 +1,10 @@
+# define provider and region
 provider "aws" {
   region = "us-east-2"
+}
+
+# create single instance in aws
+resource "aws_instance" "example" {
+  ami = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
 }
