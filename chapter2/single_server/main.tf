626be0a2cd03f455ddad6daae0ff2382d0bcd474
Diego Zarpelon <diego.zarpelon@hashicorp.com>
2025-01-22 14:55:07 +0100

    add security group to publish port 8080

--- chapter2/single_server/main.tf
+++ chapter2/single_server/main.tf
@@ -7,6 +7,8 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
+  # make the instance part of the vpc securit group
+  vpc_security_group_ids = [aws_security_group.instance.id]
   # run script to add data to index.html
   user_data = <<-EOF
     #!/bin/bash
@@ -20,3 +22,15 @@ tags = {
 }
 }
 
+# create security group to allo port 8080
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+}
