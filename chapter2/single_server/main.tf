88bdfffe0562c6df7c5de801fe59642ff0a0ee16
Diego Zarpelon <diego.zarpelon@hashicorp.com>
2025-01-22 14:47:15 +0100

    create webserver

--- chapter2/single_server/main.tf
+++ chapter2/single_server/main.tf
@@ -7,6 +7,13 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
+  # run script to add data to index.html
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello, World" > index.html
+    nohup busybox httpd -f -p 8080 &
+    EOF
+  user_data_replace_on_change = true
 #add tag to name the resource
 tags = {
   name = "terraform-example"
