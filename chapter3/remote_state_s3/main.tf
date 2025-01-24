9ff5329dd4e6c7b36509a38d247686dab00d1801
Diego Zarpelon <diego.zarpelon@hashicorp.com>
2025-01-24 16:57:17 +0100

    enable versioning in the bucket

--- chapter3/remote_state_s3/main.tf
+++ chapter3/remote_state_s3/main.tf
@@ -11,3 +11,12 @@ resource "aws_s3_bucket" "terraform_state" {
     prevent_destroy = true
   }
 }
+
+#Enable versioning of the file so full revision history of state files is possible
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
