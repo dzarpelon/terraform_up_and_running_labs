eaee04857ae85e51e5f18b4ce165ed3fad679202
Diego Zarpelon <diego.zarpelon@hashicorp.com>
2025-01-24 17:08:45 +0100

    block all public access

--- chapter3/remote_state_s3/main.tf
+++ chapter3/remote_state_s3/main.tf
@@ -31,3 +31,12 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
     }
   }
 }
+
+# Block all public access to the bucket
+
+resource "aws_s3_account_public_access_block" "public_access" {
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+  }
