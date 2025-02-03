3fd1daf840dd3578ef1794ba40a1e307fe21b38a
Diego Zarpelon <diego.zarpelon@hashicorp.com>
2025-02-03 16:23:59 +0100

    use variables file with the lenght and count loop to create the users from a list

--- chapter5/loops/main.tf
+++ chapter5/loops/main.tf
@@ -6,7 +6,10 @@ resource "aws_iam_user" "example" {
   ## for a single user, we would use the following:
   #name = "neo"
   ## as we want multiple users, if we just put the name directly it would create multiple users with same name. For this we can use the 'count.index' to create different users. here the users would be 'neo.0,neo.1,neo.2'
-  name = "neo.${count.index}"
+  #name = "neo.${count.index}"
   # as Terraform can't do for loops, we can use 'count' to create multiple users
-  count = 3
+  #count = 3
+  #using a var file to create the users from a list
+  name = var.user_names[count.index]
+  count = length(var.user_names)
 }
