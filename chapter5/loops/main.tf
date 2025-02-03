adb02bdf832e09c7b7a5d83fc7291a1784aa88ee
Diego Zarpelon <diego.zarpelon@hashicorp.com>
2025-02-03 17:15:24 +0100

    for_each usage example

--- chapter5/loops/main.tf
+++ chapter5/loops/main.tf
@@ -10,6 +10,32 @@ resource "aws_iam_user" "example" {
   # as Terraform can't do for loops, we can use 'count' to create multiple users
   #count = 3
   #using a var file to create the users from a list
-  name = var.user_names[count.index]
-  count = length(var.user_names)
+  #name = var.user_names[count.index]
+  #count = length(var.user_names)
+  #count has two limitations 1: can't be used in inline data blocks and 2: as it's positional, when we delete a count[1] item item while having a count[2] what it will actually do is change the postion of 2 to 1 while replacing it to the other part. 
+# to solve that we use for_each loops
+  for_each = toset(var.user_names) # for_each only supports sets and maps when used on a resource
+  name =  each.value
 }
+
+
+# as now the users are an array of resources, we need to specify which user we want by its position in the array:
+/*output "first_arn" {
+  value = aws_iam_user.example[0].arn
+  description = "The arn of the first user"
+}
+*/
+# we can retrieve the arn of all users by using the "*" splat sign when using count
+
+/*output "all_arns" {
+  value = aws_iam_user.example[*].arn
+  description = "The arn of each user"
+}
+*/
+
+# if using for_each, we don't need to use * to get all users arns
+output "all_users" {
+  value = values(aws_iam_user.example)[*].arn
+}
+
+
