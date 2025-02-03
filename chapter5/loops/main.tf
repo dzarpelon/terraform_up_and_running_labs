223edc7a16d102f7e19ef4c85fc82a87a73954c7
Diego Zarpelon <diego.zarpelon@hashicorp.com>
2025-02-03 16:19:04 +0100

    count loop to create multiple users

---
+++ chapter5/loops/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_iam_user" "example" {
+  ## for a single user, we would use the following:
+  #name = "neo"
+  ## as we want multiple users, if we just put the name directly it would create multiple users with same name. For this we can use the 'count.index' to create different users. here the users would be 'neo.0,neo.1,neo.2'
+  name = "neo.${count.index}"
+  # as Terraform can't do for loops, we can use 'count' to create multiple users
+  count = 3
+}
